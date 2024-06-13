8da61fdaee9a3260605b1835777fb7cc77f6f5a2
pablo <92185570+pablo756xD@users.noreply.github.com>
2024-06-13 00:21:56 -0600

    fixed path

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-libtool --mode=link gcc -o main main.c -I/home/pablo/libfprint/libfprint -L/home/pablo/libfprint/libfprint -lfprint $(pkg-config --cflags --libs gtk+-3.0)
+libtool --mode=link gcc -o main main.c -I~/libfprint/libfprint -L~/libfprint/libfprint -lfprint $(pkg-config --cflags --libs gtk+-3.0)
 
